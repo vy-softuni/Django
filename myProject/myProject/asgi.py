8990a4963de0ab94bf4623632cbd68e19645289c
vy-softuni <shark_invest@abv.bg>
2024-04-14 23:51:04 +0300

    some core operations

--- myProject/myProject/asgi.py
+++ myProject/myProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TimeOptimizer project.
+ASGI config for myProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TimeOptimizer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myProject.settings')
 
 application = get_asgi_application()
